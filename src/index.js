548b10e03abed02fd6fccfa09e9239889e1b4a87
santhossundar <santhoshsunthar@gmail.com>
2023-04-12 21:35:33 +0530

    Removed minor styles

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import './assets/css/custom.css';
+import './css/custom.css';
 
 ReactDOM.render(
   <React.StrictMode>
