22ddf3e356caddbdf561ed91a1c92e4bb1dafe7b
santhossundar <santhoshsunthar@gmail.com>
2023-04-12 21:34:51 +0530

    Added #Body section inline styles

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,9 +3,18 @@ import React from 'react';
 const Body = () => {
     return (
         <section className='body'>
-            <div className="container-fluid align-items-center">
+            <div className="container-fluid align-items-center"
+                style={{
+                    background: `linear-gradient(
+                        0deg,rgba(255, 255, 255, 0.5) 0%,
+                        rgba(255, 255, 255, 0.2) 48%,
+                        rgba(255, 255, 255, 0.2) 77%,
+                        rgba(236, 252, 255, 0.5) 100%), 
+                        url(/images/sql.png) no-repeat`,
+                    backgroundSize: 'cover'
+                }}>
                 <div className="card mb-5 mt-5 shadow-sm">
-                    <img src={require('../assets/images/sql.png')} alt="Card Image" className='img-fluid' />
+                    <img src="/images/sql.png" alt="Card Image" className='img img-fluid' />
 
                     <div className="card-body">
                         <div className="card-title">
@@ -16,9 +25,18 @@ const Body = () => {
                 </div>
             </div>
 
-            <div className="container-fluid align-items-center">
+            <div className="container-fluid align-items-center"
+                style={{
+                    background: `linear-gradient(
+                    0deg,rgba(255, 255, 255, 0.5) 0%,
+                    rgba(255, 255, 255, 0.2) 48%,
+                    rgba(255, 255, 255, 0.2) 77%,
+                    rgba(236, 252, 255, 0.5) 100%), 
+                    url(/images/tableau.png) no-repeat`,
+                    backgroundSize: 'cover'
+                }}>
                 <div className="card mb-5 mt-5 shadow-sm">
-                    <img src={require('../assets/images/sql.png')} alt="Card Image" className='img-fluid' />
+                    <img src="/images/tableau.png" alt="Card Image" className='img img-fluid' />
 
                     <div className="card-body">
                         <div className="card-title">
@@ -29,16 +47,24 @@ const Body = () => {
                 </div>
             </div>
 
-            <div className="container-fluid align-items-center">
+            <div className="container-fluid align-items-center"
+                style={{
+                    background: `linear-gradient(
+                    0deg,rgba(255, 255, 255, 0.5) 0%,
+                    rgba(255, 255, 255, 0.2) 48%,
+                    rgba(255, 255, 255, 0.2) 77%,
+                    rgba(236, 252, 255, 0.5) 100%), 
+                    url(/images/sql.png) no-repeat`,
+                    backgroundSize: 'cover'
+                }}>
                 <div className="card mb-5 mt-5 shadow-sm">
-                    <img src={require('../assets/images/sql.png')} alt="Card Image" className='img-fluid' />
+                    <img src="/images/sql.png" alt="Card Image" className='img img-fluid' />
 
                     <div className="card-body">
                         <div className="card-title">
                             Project Name
-                        </div>
+                        </div>       
                     </div>
-
                 </div>
             </div>
         </section>
