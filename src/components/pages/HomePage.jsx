1581b8fe3b73f54a0291a88789743d21c268792c
santhossundar <santhoshsunthar@gmail.com>
2023-04-13 00:37:35 +0530

    Added major changes

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Body from '../Body';
 import Footer from '../Footer';
 import Header from '../Header';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const HomePage = () => {
+    useEffect(() => {
+        AOS.init({ duration: 1000 });
+    }, []);
 
     return (
         <div className='App'>
             <Header />
             <Body />
-            <Footer/>
+            <Footer />
         </div>
     )
 }
