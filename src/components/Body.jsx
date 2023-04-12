4c157a6d44683ae5ca125a192042f2960320392f
santhossundar <santhoshsunthar@gmail.com>
2023-04-12 13:57:59 +0530

    Moved images directory into public

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import CardImg from '../assets/images/sql.png';
 
 const Body = () => {
     return (
         <section className='body'>
             <div className="container-fluid align-items-center">
                 <div className="card mb-5 mt-5 shadow-sm">
-                    <img src={CardImg} alt="Card Image" className='img-fluid' />
+                    <img src={require('../assets/images/sql.png')} alt="Card Image" className='img-fluid' />
 
                     <div className="card-body">
                         <div className="card-title">
@@ -19,7 +18,7 @@ const Body = () => {
 
             <div className="container-fluid align-items-center">
                 <div className="card mb-5 mt-5 shadow-sm">
-                    <img src={CardImg} alt="Card Image" className='img-fluid' />
+                    <img src={require('../assets/images/sql.png')} alt="Card Image" className='img-fluid' />
 
                     <div className="card-body">
                         <div className="card-title">
@@ -32,7 +31,7 @@ const Body = () => {
 
             <div className="container-fluid align-items-center">
                 <div className="card mb-5 mt-5 shadow-sm">
-                    <img src={CardImg} alt="Card Image" className='img-fluid' />
+                    <img src={require('../assets/images/sql.png')} alt="Card Image" className='img-fluid' />
 
                     <div className="card-body">
                         <div className="card-title">
