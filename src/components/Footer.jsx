1cd9093f7c888b227b27b0fb5e082ffab64b1b1c
santhossundar <santhoshsunthar@gmail.com>
2023-04-12 12:55:51 +0530

    Added #Footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <section className='footer'>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-lg-6 col-md-6">
+                        Quick links
+                    </div>
+
+                    <div className="col-lg-6 col-md-6">
+                        copyright
+                    </div>
+
+                    <div className="col-lg-6 col-md-6">
+                        Social links
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Footer
