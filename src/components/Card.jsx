a17bf5c35eb150a0e3aa12d30b51647e2932646e
santhossundar <santhoshsunthar@gmail.com>
2023-04-13 20:28:13 +0530

    Added #Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { motion } from 'framer-motion';
+
+const Card = ({imgName, projectName}) => {
+    return (
+        <div className="card mb-5 mt-5 shadow-sm">
+            <motion.img
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1, transition: { duration: 2 } }}
+                src={"/images/"+imgName}
+                alt="Card Image"
+                className='img img-fluid'
+            />
+
+            <div className="card-body">
+                <div className="card-title"
+                >
+                    {projectName}
+                </div>
+
+                <motion.div
+                    initial={{ opacity: 0, translateY: 100 }}
+                    whileInView={{ opacity: 1, translateY: 0, transition: { duration: 0.5 } }}
+                    className="view-more">
+                    <a href="#" className='card-btn'>
+                        View More
+                    </a>
+                </motion.div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
