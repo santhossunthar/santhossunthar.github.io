dfe8b4c2dda874b7ab9c68de7191176bfed2b59f
santhossunthar <santhossunthar@users.noreply.github.com>
2025-10-28 00:01:56 +0000

    deploy: e26432aac0ca87dc6e9f93bae30c7644195cc6df

---
+++ _next/static/8o_9PV4xTyaUG9iKPO8Vw/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/Home": [
+    "static/chunks/ccff473f935736ef.js"
+  ],
+  "/_error": [
+    "static/chunks/64ecd8168031d23f.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/Home",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/2c32ac3c360bea03.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/c68f7b4bc8ba66b9.js",
+  "static/chunks/bdd04eb65e16f17f.js",
+  "static/chunks/turbopack-c455d0601c08e4cd.js"
+])
